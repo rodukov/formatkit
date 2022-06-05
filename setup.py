0c8a9fe19a62c80aedcdfaee7f887128a5385c7b
rodukov <electroneuphoria@gmail.com>
2022-06-05 23:55:08 +0300

    formatkit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.0.0'
+VERSION = '1.0.3'
 DESCRIPTION = 'Great terminal formatting tool'
 LONG_DESCRIPTION = 'A package that can color text, fill text, and format text'
 
 # Setting up
 setup(
-    name="pythonpackage",
+    name="formatkit",
     version=VERSION,
     description=DESCRIPTION,
     packages=find_packages(),
